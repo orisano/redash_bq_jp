41282c9f9c462a9a8f16d7f5b196ebcfc7b8ad62
Nao Yonashiro <owan.orisano@gmail.com>
2017-09-07 04:47:24 +0900

    fix: fix regex

--- redash_bq_jp.py
+++ redash_bq_jp.py
@@ -8,7 +8,7 @@ import six
 
 
 class BigQueryJP(BigQuery):
-    JAPANESE_MATCHER = re.compile(six.u("(?<=as )[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]+(?=[\\s,\\Z])"))
+    JAPANESE_MATCHER = re.compile(six.u("(?<=as)\\s+[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]+"))
 
     @classmethod
     def type(cls):
@@ -19,7 +19,7 @@ class BigQueryJP(BigQuery):
             return six.u("_") + hashlib.md5(s.encode("utf-8")).hexdigest()
 
         escape_table = {
-            escape(japanese): japanese
+            escape(japanese.lstrip()): japanese.lstrip()
             for japanese in JAPANESE_MATCHER.finditer(query)
         }
 
