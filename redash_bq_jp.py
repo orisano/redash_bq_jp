dd7104596d89fd14d49d8060f8797955ef7337b3
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2017-09-07 15:21:32 +0900

    fix: fix JAPANESE_MATCHER

--- redash_bq_jp.py
+++ redash_bq_jp.py
@@ -8,7 +8,7 @@ import six
 
 
 class BigQueryJP(BigQuery):
-    JAPANESE_MATCHER = re.compile(six.u("(?<=as)\\s+[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]+"))
+    JAPANESE_MATCHER = re.compile(six.u("(?<=\\s(as|aS|As|AS)\\s)\\s*\\S*[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]+[^\\s;,]"))
 
     @classmethod
     def type(cls):
